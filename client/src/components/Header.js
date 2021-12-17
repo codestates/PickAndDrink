5321ab2733ff7def06991b0673f0267b292a0116
LeeTaeGwan <freshman1998@naver.com>
2021-12-17 13:33:27 +0900

    server sync

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import "./Header.css"
+
+function Header() {
+return (
+  <div>
+    <header>
+      <div>logo</div>
+      <div>search</div>
+      <div>signin</div>
+    </header>
+
+    <nav>
+      <div>story</div>
+      <div>음료 랭킹</div>
+      <div>편의점 음료 특가</div>
+      <div>mypage</div>
+    </nav>
+  </div>
+)
+}
+
+export default Header;
