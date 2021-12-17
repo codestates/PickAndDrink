709ed9610111cba2bb40c87c5567ee7caf9c0980
LittleBiber <rlatkdgud98@naver.com>
2021-12-17 15:41:10 +0900

    add: DB test data

---
+++ server/seeders/5-demo-like-comment.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('like_comment', [
+      {
+        like: 1,
+        comment: "testComment1ForZeroCola",
+        user_id: 1,
+        item_id: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        like: 1,
+        comment: "testComment1ForTea",
+        user_id: 1,
+        item_id: 2,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        like: 1,
+        comment: "testComment1ForCoffee",
+        user_id: 2,
+        item_id: 3,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+  ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('like_comment', null, {});
+  }
+};
