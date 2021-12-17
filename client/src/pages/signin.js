4b85533acbee5f17e0cb7d3bd4402486418b5952
songdayoung <tjs2446@gmail.com>
2021-12-17 16:29:00 +0900

    component

---
+++ client/src/pages/signin.js
@@ -0,0 +1,14 @@
+// import React, { useState } from 'react';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+
+function Singin (props) { 
+    return (
+      <div>
+       <h1>signin</h1>
+      </div>
+    );
+  }
+  
+  export default Singin;
