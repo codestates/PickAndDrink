12f8b6af3aec98c0c11d66d6c99bb821d34437e9
racyde <racyde.naver.com>
2021-12-18 18:44:31 +0900

    add auth/me.js
    me 구현 했고, API 문서를 일부 수정함(res에 userdata만 응답하도록 변경 -> 액세스토큰 부분은 삭제)

--- server/router.js
+++ server/router.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // auth routing
 router.post('/auth/login', controllers.login)
 router.get('/auth/logout', controllers.logout)
-// router.get('/auth/me', controllers.me)
+router.get('/auth/me', controllers.me)
 router.post('/auth/signup', controllers.signup)
 router.delete('/auth/signout', controllers.signout)
 
