4c8a42c46d921b8834fcf26a623333f2db855047
racyde <racyde.naver.com>
2021-12-20 10:05:34 +0900

    Dev: controller부 작성(item, like) 및 router.js 코드 수정

--- server/router.js
+++ server/router.js
@@ -18,6 +18,7 @@ router.delete('/auth/signout', controllers.signout)
 // router.get('/comment/:item-id', controllers.comment.getItemComment) // getItemComment 라는 함수명으로 가져오는 것으로 임시 작성
 
 // //item routing
+router.get('/item', controllers.item) // 기본값? 기본메서드 같은 느낌?
 // router.get('/item', controllers.item.sortByRanking) // 기본값? 기본메서드 같은 느낌?
 // router.get('/item', controllers.item.findByType)
 // router.get('/item', controllers.item.findByStore)
@@ -27,14 +28,14 @@ router.delete('/auth/signout', controllers.signout)
 // router.put('/item/:item-id/comment/:comment-id', controllers.item.putComment) //! API 문서 보완 필요
 // router.delete('/item/:item-id/comment/:comment-id', controllers.item.deleteComment)  //! 뭔가 API쪽의 보완 작업이 필요할 것으로 생각됨 
 
-// router.get('/item/:user-id/like', controllers.item.findLike)    //! 해당 메서드는 로직 고민이 필요
-// // 특정 사용자가 좋아요 누른 아이템 목록들을 전체 조회??가 필요?
-// router.post('/item/:item-id/like', controllers.item.postLike)
-// router.delete('/item/:item-id/like', controllers.item.deleteLike)
-
-
 // // like routing
 // !API 문서 보완 필요할듯?
+router.get('/like', controllers.like.findLike)    //! 해당 메서드는 로직 고민이 필요
+// // 특정 사용자가 좋아요 누른 아이템 목록들을 전체 조회??가 필요?
+// router.post('/item/:item-id/like', controllers.like.postLike)
+// router.delete('/item/:item-id/like', controllers.like.deleteLike)
+
+
 
 
 router.get('/', (req, res) => {
