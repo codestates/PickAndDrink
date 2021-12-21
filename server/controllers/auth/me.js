f548ab70f1755b8ccc01338dd7819b4cfdf5d32c
racyde <racyde.naver.com>
2021-12-21 17:20:09 +0900

    temp: auth/me 수정중

--- server/controllers/auth/me.js
+++ server/controllers/auth/me.js
@@ -1,39 +1,42 @@
 const { user } =require('../../models')
 const { chkValid } =require('../tokenFunctions')
 
-module.exports = (req, res) => {
+module.exports = {
 
-if(!req.headers.authorization){
-    res.status(400).send({  message: "Bad Request"})
-}
-
-else{
-
-    const accessTokenData = chkValid(req);
-
-    if(accessTokenData===null){
-        res.status(401).send({ message: "Invalid token" }); 
-      }
-
-    //   console.log(accessTokenData)
-      const {email} = accessTokenData;
-
-      user.findOne({ where: {email} })
-      .then( (data) =>{
-        if(!data){
-          res.status(400).send({ message: "존재하지 않는 사용자입니다"  });
-        }
-
-        const {id, username, email, nickname, createdAt, updatedAt} = data.dataValues;
-        const payLoad = {id, username, email, nickname, createdAt, updatedAt};
-
-        // console.log(data)
-        res.status(200).json({"userData": payLoad})
+  getMyInfo: async (req, res) => {
     
-    }).catch((err) =>
-        // console.log("내부 에러 발생")
-        res.status(500).send({ message: "Internel Server Error" })
-        )
+    if(!req.headers.authorization){
+        res.status(400).send({  message: "Bad Request"})
     }
+    
+    else{
+    
+        const accessTokenData = chkValid(req);
+    
+        if(accessTokenData===null){
+            res.status(401).send({ message: "Invalid token" }); 
+          }
+    
+        //   console.log(accessTokenData)
+          const {email} = accessTokenData;
+    
+          user.findOne({ where: {email} })
+          .then( (data) =>{
+            if(!data){
+              res.status(400).send({ message: "존재하지 않는 사용자입니다"  });
+            }
+    
+            const {id, username, email, nickname, createdAt, updatedAt} = data.dataValues;
+            const payLoad = {id, username, email, nickname, createdAt, updatedAt};
+    
+            // console.log(data)
+            res.status(200).json({"userData": payLoad})
+        
+        }).catch((err) =>
+            // console.log("내부 에러 발생")
+            res.status(500).send({ message: "Internel Server Error" })
+            )
+        }
+  }
 
 }
