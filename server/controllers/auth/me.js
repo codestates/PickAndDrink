db92ea58756af6e2a4250bc1669cc177059f89fb
racyde <racyde.naver.com>
2021-12-21 18:23:59 +0900

    Dev: 회원정보 수정 기능 구현
    /auth/me.js 수정 및  router.js 수정

--- server/controllers/auth/me.js
+++ server/controllers/auth/me.js
@@ -37,6 +37,56 @@ module.exports = {
             res.status(500).send({ message: "Internel Server Error" })
             )
         }
+  },
+
+  updateUserInfo: async(req, res) => {
+    // 현재 nickname만 정보수정 가능
+    // console.log(req.body)
+    const {nickname} = req.body
+
+    if(!req.headers.authorization){
+      res.status(400).send({  message: "Bad Request"})
+    }
+    
+    else{
+      
+      const accessTokenData = chkValid(req);
+      
+      if(accessTokenData===null){
+        res.status(401).send({ message: "Invalid token" }); 
+      }
+      
+      try {
+        // console.log(accessTokenData)
+        // 기존과 동일한 별명인지 확인해야 하는 작업 필요할듯?
+        const checkNickname = await user.findOne({
+          where: {nickname}
+        })
+
+        if(checkNickname){  // 기존에 사용중인 닉네임인 경우 409: Conflict
+          res.status(409).send({message: "already used nickname. check your nickname"})
+        }
+        else{
+
+          const updateUser = await user.update({ nickname },
+            {
+              where: {email: accessTokenData.email}
+            })
+          
+          const newNicknameInfo = await user.findOne({
+            where: {nickname},
+            raw: true,
+          })
+          delete newNicknameInfo.password
+            // console.log(newNicknameInfo)
+            res.status(200).json({ data: newNicknameInfo, message: "complete" })
+            
+          }
+      } catch (error) {
+        res.status(500).send({ message: "Internel Server Error" })
+      }
+    }
+
   }
 
 }
--- server/router.js
+++ server/router.js
@@ -10,6 +10,7 @@ require('dotenv').config()
 router.post('/auth/login', controllers.login)
 router.get('/auth/logout', controllers.logout)
 router.get('/auth/me', controllers.me.getMyInfo)
+router.patch('/auth/me', controllers.me.updateUserInfo)
 router.post('/auth/signup', controllers.signup)
 router.delete('/auth/signout', controllers.signout)
 
